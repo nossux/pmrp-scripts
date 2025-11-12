528427f5504327fbd8d2e5a442ca0ab1b545604a
nossux <contact.nossux@gmail.com>
2025-11-12 21:45:31 +0000

    Remove LSPD heli cam

--- heli/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
--- Manifest
-author 'mraes | https://forum.cfx.re/u/mraes/summary'
-description 'FiveM LSPD Heli Cam by mraes'
-
-client_script 'heli_client.lua'
-server_script 'heli_server.lua'
